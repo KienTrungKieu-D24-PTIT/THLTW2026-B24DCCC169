eae9ce71052c35119b4eff2e002e23c92a9096d4
KienTrungKieu-D24-PTIT <KienKT.B24CC169@stu.ptit.edu.vn>
2026-03-04 16:40:49 +0700

    Update hoctap.ts

--- src/models/hoctap.ts
+++ src/models/hoctap.ts
@@ -1,9 +1,7 @@
 import { useState } from 'react';
-
 export default () => {
   const [danhMucMonHoc] = useState<string[]>(['Toán', 'Văn', 'Anh', 'Khoa học', 'Công nghệ']);
   const [lichSuHocTap] = useState([]);
-
   return {
     danhMucMonHoc,
     lichSuHocTap,
